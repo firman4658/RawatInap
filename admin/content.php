c311df69d145c03d6e287c38f2cdb7e21ba09a84
firman4658 <firmansyahbolong@gmail.com>
2021-03-25 07:31:31 -0700

    Update content.php

--- admin/content.php
+++ admin/content.php
@@ -3,37 +3,14 @@
 		echo"hai";
 	}
 
-	//bagian produk
-	elseif ($_GET['module']=='produk') 
+	//bagian dokter
+	elseif ($_GET['module']=='dokter') 
 	{
-		include "module/produk/produk.php";
+		include "module/dokter/dokter.php";
 	}
-	elseif ($_GET['module']=='kategori') 
+	elseif ($_GET['module']=='pasien') 
 	{
-		include "module/kategori/kategori.php";
+		include "module/pasien/pasien.php";
 	}
-	elseif ($_GET['module']=='provinsi') 
-	{
-		include "module/provinsi/provinsi.php";
-	}
-	elseif ($_GET['module']=='kota') 
-	{
-		include "module/kota/kota.php";
-	}
-	elseif ($_GET['module']=='keranjang') 
-	{
-		include "module/keranjang/keranjang.php";
-	}
-	elseif ($_GET['module']=='admin') 
-	{
-		include "module/admin/admin.php";
-	}
-	elseif ($_GET['module']=='kecamatan') 
-	{
-		include "module/kecamatan/kecamatan.php";
-	}
-	elseif ($_GET['module']=='det_pembelian') 
-	{
-		include "module/det_pembelian/det_pembelian.php";
-	}
+
 ?>

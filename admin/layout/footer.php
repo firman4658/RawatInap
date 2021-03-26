8fcc23578a57a438ee67fa5c7b14729ee5f179bb
firman4658 <firmansyahbolong@gmail.com>
2021-03-25 22:59:49 -0700

    ss

--- admin/layout/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="http://adminlte.io">NHYSHOP</a>.</strong>
-    All rights reserved.  | Design by KELOMPOK 1
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0.0
-    </div>
-  </footer>

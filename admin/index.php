7bdc3b63b4be899c1ce9cc18150bd7d2f24075c9
firman4658 <firmansyahbolong@gmail.com>
2021-03-28 00:07:41 -0700

    sa

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,16 @@
 <html>
 <head>
-<title>Login Form Design</title>
+<title>Login Dokter</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 <body>
     <div class="loginbox">
     <img src="avatar.png" class="avatar">
         <h1>Login Here</h1>
-        <form>
+        <form action="cek_login.php" method="post">
             <p>Username</p>
-            <input type="text" name="" placeholder="Enter Username">
+            <input type="text" name="" placeholder="Enter Username" required="required">
             <p>Password</p>
-            <input type="password" name="" placeholder="Enter Password">
+            <input type="password" name="" placeholder="Enter Password" required="required">
             <input type="submit" name="" value="Login">
             <a href="#">Lost your password?</a><br>
             <a href="#">Don't have an account?</a>

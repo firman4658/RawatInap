cb6561ba8ae26516f65fda017ed7a7a9bf098536
firman4658 <firmansyahbolong@gmail.com>
2021-03-27 23:30:38 -0700

    dssd

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
 	<div class="login-box">
 		<img src="avatar.png" class="avatar">
 		<h1>Login Here</h1>
-		<form>
+		<form action="cek_login.php" method="post"></form>
 			<input type="text" name="Username" placeholder="Enter Username">
 			<p>Password</p>
 			<input type="Password" name="Password" placeholder="Enter Password">

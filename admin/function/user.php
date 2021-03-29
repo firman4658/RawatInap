38eb4f38d6b5c2220e3961310c38247334297676
aswarthedoctor@gmail.com <68291451+Aswar12@users.noreply.github.com>
2021-03-29 13:58:09 +0800

    Update user.php

--- admin/function/user.php
+++ admin/function/user.php
@@ -1,5 +1,4 @@
 <?php
-include '../admin/home.php';
 include '../config/koneksi.php';
         $query = mysqli_query($konek, "SELECT * FROM dokter WHERE username = '".$id."'");
         $data = mysqli_fetch_array($query);

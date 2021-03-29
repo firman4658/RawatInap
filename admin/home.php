8527c754eb5a8408459db7de3220896846b69d1e
aswarthedoctor@gmail.com <68291451+Aswar12@users.noreply.github.com>
2021-03-29 14:55:09 +0800

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -88,7 +88,10 @@ if(isset($_COOKIE['username']) && $_COOKIE['username'] != ''){
                   <li><a href="?module=pasien" class="nav-link"><i class="fa fa-users"></i> Pasien</a>
                    
                   </li>
-                  <li><a href="?module=kamar" class="nav-link"><i class="fa fa-table"></i> Kamar </a>
+                  <li><a href="?module=kamar" class="nav-link"><i class="fa fa-bed"></i> Kamar </a>
+                  </li>
+
+                  <li><a href="?module=diagnosa" class="nav-link"><i class="fa fa-stethoscope"></i> Diagnosa</a>
                   </li>
 
                   <li><a href="?module=pembayaran" class="nav-link"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Pembayaran </a>

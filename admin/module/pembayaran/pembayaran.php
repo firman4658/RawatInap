9c7a9f31726695f0cc8c1bff569f668cb9daae80
aswarthedoctor@gmail.com <68291451+Aswar12@users.noreply.github.com>
2021-04-06 15:14:35 +0800

    Update pembayaran.php

--- admin/module/pembayaran/pembayaran.php
+++ admin/module/pembayaran/pembayaran.php
@@ -3,20 +3,20 @@ $aksi = "module/pembayaran/aksi_pembayaran.php";
 
 switch ($_GET['act']) {
 
-     default:
-          // Tampil Data - mengambil file adminshow.php
-          echo "<a href='?module=pembayaran&act=tambahuser' class='nav-link text-black'><i class='fa fa-plus-circle fa-lg'></i> Tambah</a>
+	default:
+		// Tampil Data - mengambil file adminshow.php
+		echo "<a href='?module=pembayaran&act=tambahuser' class='nav-link text-black'><i class='fa fa-plus-circle fa-lg'></i> Tambah</a>
 		<table cellpadding='40px' id='rawat' class='table table-striped jambo_table bulk_action table-bordered align-middle' cellspacing='0' width='120%'>
 		 <thead align=center>
 			<tr>
 				<th>NO</th> <th>Nama Pasien</th> <th>Tgl Keluar</th> <th>Biaya Tindakan</th> <th>Jenis Asuransi</th> <th>Total Bayar</th>  <th>Pilihan</th> 
 			</tr></thead>";
-          $no = 0;
+		$no = 0;
 
-          $data = mysqli_query($konek, "SELECT * FROM pembayaran inner join diagnosa on diagnosa.id_diagnosa=pembayaran.id_diagnosa inner join pasien on pasien.id_pasien=diagnosa.id_pasien");
-          while ($r = mysqli_fetch_array($data)) {
-               $no++;
-               echo "<tr align=center>
+		$data = mysqli_query($konek, "SELECT * FROM pembayaran inner join diagnosa on diagnosa.id_diagnosa=pembayaran.id_diagnosa inner join pasien on pasien.id_pasien=diagnosa.id_pasien");
+		while ($r = mysqli_fetch_array($data)) {
+			$no++;
+			echo "<tr align=center>
 				<td>$no</td> <td>$r[nama_pasien]</td> <td>$r[tgl_keluar]</td> <td>$r[biaya_tindakan]</td> <td>$r[nama_asuransi]</td>   
 				<td align='center'>
 				<a href='?module=pembayaran&act=hitung&id=$r[id_pembayaran]'><input class='btn btn-success' type=submit name=hitung value='Hitung'></a>
@@ -25,13 +25,13 @@ switch ($_GET['act']) {
                     <a href='$aksi?module=pembayaran&act=hapus&id=$r[id_pembayaran]'> <i class='fa fa-trash-o fa-2x' aria-hidden='true'></i> </a>
                </td>
           </tr>";
-          }
-          echo "</table>";
-          break;
+		}
+		echo "</table>";
+		break;
 
-          // Tambah Data - memanggil file pasienfm.php
-     case "tambahuser":
-          echo "<form action='$aksi?module=pembayaran&act=input' method='POST'>
+		// Tambah Data - memanggil file pasienfm.php
+	case "tambahuser":
+		echo "<form action='$aksi?module=pembayaran&act=input' method='POST'>
 			<table class='table table-striped table-bordered' >
 				<tr>
 					<td>Nama Pasien</td> <td><input class='form-control' type=text name=nama_pasien></td>
@@ -79,15 +79,15 @@ switch ($_GET['act']) {
 				</tr>
 			</table>
 		</form>";
-          break;
+		break;
 
-          // Edit Data - memanggil file pasieneditfm.php
-     case "edituser":
+		// Edit Data - memanggil file pasieneditfm.php
+	case "edituser":
 
-          $data = mysqli_query($konek, "SELECT * FROM pasien where id_pasien='$_GET[id]'");
-          $r = mysqli_fetch_array($data);
+		$data = mysqli_query($konek, "SELECT * FROM pasien where id_pasien='$_GET[id]'");
+		$r = mysqli_fetch_array($data);
 
-          echo "<form action='$aksi?module=pasien&act=update' method='POST'>
+		echo "<form action='$aksi?module=pasien&act=update' method='POST'>
 			<table class='table table-striped table-bordered'>
 				<tr>
 					<td>id Pasien</td> 
@@ -142,35 +142,43 @@ switch ($_GET['act']) {
 				</tr>
 			</table>
 		</form>";
-          break;
-		
-		  case "hitung":
-			$data = mysqli_query($konek, "SELECT * FROM pembayaran inner join diagnosa on pembayaran.id_diagnosa=diagnosa.id_diagnosa inner join asuransi on asuransi.nama_asuransi=pembayaran.nama_asuransi INNER JOIN pasien ON pasien.id_pasien=diagnosa.id_pasien inner join kamar on kamar.id_kamar=diagnosa.id_kamar where id_pembayaran='$_GET[id]'");
-			while ($r = mysqli_fetch_assoc($data)) {
-		  
-		  
-			  $no++;
-			  $pasien = $r['nama_pasien'];
-			  $kamar = $r['harga'];
-			  $tindakan = $r['biaya_tindakan'];
-			  $asuransi = $r['potongan'];
-		  
-		  
-			  $hasil = $kamar + $tindakan - $asuransi;
-			}
-			echo " 
+		break;
+
+	case "hitung":
+		$data = mysqli_query($konek, "SELECT * FROM pembayaran inner join diagnosa on pembayaran.id_diagnosa=diagnosa.id_diagnosa inner join asuransi on asuransi.nama_asuransi=pembayaran.nama_asuransi INNER JOIN pasien ON pasien.id_pasien=diagnosa.id_pasien inner join kamar on kamar.id_kamar=diagnosa.id_kamar where id_pembayaran='$_GET[id]'");
+		while ($r = mysqli_fetch_assoc($data)) {
+
+
+			$no++;
+			$tgl1 = $r['tgl_daftar'];
+			$tgl2 = $r['tgl_keluar'];
+
+			$usia = new DateTime($tgl1);
+			$sekarang = new DateTime($tgl2);
+			$selisi = $sekarang->diff($usia);
+
+			$pasien = $r['nama_pasien'];
+			$biaya = ($r['harga']);
+			$kamar = ($r['harga']  * 1000) * ($selisi->d);
+			$tindakan = ($r['biaya_tindakan'] * 1000);
+			$asuransi = ($r['potongan'] * 1000);
+
+
+
+
+			$hasil = $kamar + $tindakan - $asuransi;
+		}
+		echo "   <br>	
 			<h2 align=center> RINCIAN PEMBAYARAN RUMAH SAKIT RAWAT INAP</h2>";
-			echo " 
-			<table cellpadding='40px' class='table table-striped jambo_table bulk_action table-bordered align-middle' cellspacing='0' width='150%'>
+		echo " 
+			<table cellpadding='40px' class='table table-striped jambo_table bulk_action table-bordered align-middle' cellspacing='0' >
 			<thead align=center>
-				 <th>Nama Pasien</th> <th>Biaya Tindakan</th> <th>Biaya Kamar</th> <th>Potongan</th> <th>Total Bayar</th> 
+				 <th>Nama Pasien</th> <th>Biaya Tindakan</th><th>Tarif kamar/Hari </th><th>Lama Inap</th> <th>Biaya Kamar</th> <th>Potongan</th> <th>Total Bayar</th> 
 			</tr></thead>";
-			echo "<tr align=center>
-						<td>$pasien</td> <td>Rp.$tindakan</td> <td>Rp.$kamar</td> <td>Rp.$asuransi</td> <td>Rp.$hasil.000</td>
+		echo "<tr align=center>
+			<td>$pasien</td> <td>Rp.$tindakan</td> <td>Rp.$biaya</td><td>$selisi->d Hari</td><td>Rp.$kamar</td> <td>Rp.$asuransi</td> <td>Rp.$hasil</td>
 				 </tr> 
 			</table>";
-			
-		   break;
 
-
+		break;
 }

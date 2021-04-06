54963bc583ffadc3cd383ecd4e09da9a9f3d8c46
firman4658 <firmansyahbolong@gmail.com>
2021-04-06 07:50:48 -0700

    sas

--- admin/module/pembayaran/pembayaran.php
+++ admin/module/pembayaran/pembayaran.php
@@ -164,9 +164,17 @@ switch ($_GET['act']) {
 			$asuransi = ($r['potongan'] * 1000);
 			$hasil = $kamar + $tindakan - $asuransi;
 
+
+			//functionfunction rupiah($angka){
+				function rupiah($angka){
+	
+					$hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+					return $hasil_rupiah;
+				}
+				echo rupiah(100000);
+
 			echo "   <br>	
-			<h2 align=center> RINCIAN PEMBAYARAN RUMAH SAKIT RAWAT INAP</h2>;
-			<p></p>";
+			<h2 align=center> RINCIAN PEMBAYARAN RUMAH SAKIT RAWAT INAP</h2>";
 			echo " 
 			<table cellpadding='40px' class='table table-striped jambo_table bulk_action table-bordered align-middle' cellspacing='0' >
 			<thead align=center>

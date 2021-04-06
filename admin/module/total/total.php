7202f8c681d165320d9466c4d8c0706dacf1e904
aswarthedoctor@gmail.com <68291451+Aswar12@users.noreply.github.com>
2021-04-06 13:15:12 +0800

    Revert "b"
    This reverts commit 91696ae23c68b65d0983901f5d1420cf82efac8f.

--- admin/module/total/total.php
+++ admin/module/total/total.php
@@ -5,8 +5,7 @@
 
   <?php
   include '../config/koneksi.php';
-  $id_pembayaran = $_GET['id_pembayaran'];
-  $data = mysqli_query($konek, "SELECT * FROM pembayaran inner join diagnosa on pembayaran.id_diagnosa=diagnosa.id_diagnosa inner join asuransi on asuransi.nama_asuransi=pembayaran.nama_asuransi INNER JOIN pasien ON pasien.id_pasien=diagnosa.id_pasien inner join kamar on kamar.id_kamar=diagnosa.id_kamar WHERE id_pembayaran = '$id_pembayaran'");
+  $data = mysqli_query($konek, "SELECT * FROM pembayaran inner join diagnosa on pembayaran.id_diagnosa=diagnosa.id_diagnosa inner join asuransi on asuransi.nama_asuransi=pembayaran.nama_asuransi INNER JOIN pasien ON pasien.id_pasien=diagnosa.id_pasien inner join kamar on kamar.id_kamar=diagnosa.id_kamar");
   while ($r = mysqli_fetch_assoc($data)) {
 
 
@@ -14,7 +13,6 @@
     $kamar = $r['harga'];
     $tindakan = $r['biaya_tindakan'];
     $asuransi = $r['potongan'];
-    $pasien = $r['nama_pasien'];
 
 
     $hasil = $kamar + $tindakan - $asuransi;
@@ -22,10 +20,9 @@
   echo " 
   <p></p>
   <tr>
-  Nama pasien     = $pasien <br>
   Biaya tindakan  = Rp.$tindakan <br>
 
-  Biaya Kamar     = Rp.$kamar <br>
+  Biaya Kamar = Rp.$kamar <br>
   Potongan dari asuransi = Rp.$asuransi <br>
 	<td>Jadi Total Yang di Bayar Pasien Adalah = Rp $hasil.000</td> 
   </tr>
